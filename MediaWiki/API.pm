7651bceeaded117886ad2c8d0aad41c9f721a65a
Jools Wills <buzz@exotica.org.uk>
2008-06-15 16:09:23 +0000

    login function and moe cleanups/better error checking

--- MediaWiki/API.pm
+++ MediaWiki/API.pm
@@ -11,6 +11,8 @@ use Data::Dumper;
 sub ERR_NO_ERROR { 0 }
 sub ERR_CONFIG { 1 }
 sub ERR_HTTP { 2 }
+sub ERR_API { 3 }
+sub ERR_LOGIN { 4 }
 
 sub new {
 
@@ -41,36 +43,58 @@ sub api {
   #print Dumper ($response->content);
 
   my $ref = XML::Simple->new()->XMLin($response->content, ForceArray => 0, KeyAttr => [ ] );
+
+  return $self->_error(ERR_API,$ref->{error}->{code}) if exists ( $ref->{error} );
+
   return $ref;
 }
 
+sub login {
+  my ($self,$query) = @_;
+  $query->{action} = 'login';
+  if ( my $ref = $self->api( $query ) ) {
+    $ref=$ref->{login};
+    return $self->_error( ERR_LOGIN, 'Login Failure: ' . $ref->{result} . ' - ' . $ref->{details} ) if ( $ref->{result} ne 'Success' );
+    return $ref;
+  } else {
+    return undef;
+  }
+}
+
+# parameters:
+# mediawiki api parameters for list functions (http://www.mediawiki.org/wiki/API:Query_-_Lists) as hashref
+# number of items to return or 0 for all
 sub list {
   my ($self, $query, $max) = @_;
-  my ($results, @list_array);
+  my ($ref, @results);
   my ($cont_key, $cont_value, $array_key, $count);
 
   my $list = $query->{list};
 
   my $do_continue = 0;
   do {
-    $results = $self->api( $query );
+    $ref = $self->api( $query );
 
+    # check if we have yet the key which contains the array of hashrefs for the ref
+    # if not, then get it.
     if ( ! defined( $array_key ) ) { 
-      ($array_key,)=each( %{ $results->{query}->{$list} } );
+      ($array_key,)=each( %{ $ref->{query}->{$list} } );
     }
-    $count +=  scalar @{$results->{query}->{$list}->{$array_key}};
+    # count the items in the array, and add to our total
+    $count +=  scalar @{$ref->{query}->{$list}->{$array_key}};
 
-    if ( exists( $results->{'query-continue'} ) ) {
-      # get query-continue hashref and extract key and value (key will be used as from parameter)
-      ($cont_key, $cont_value) = each( %{ $results->{'query-continue'}->{$list} } );
+    # check if there are more ref to be had
+    if ( exists( $ref->{'query-continue'} ) ) {
+      # get query-continue hashref and extract key and value (key will be used as from parameter to continue where we left off)
+      ($cont_key, $cont_value) = each( %{ $ref->{'query-continue'}->{$list} } );
       $query->{$cont_key} = $cont_value;
-      $do_continue=1;
+      $do_continue = 1;
     }
-    push @list_array, @{$results->{query}->{$list}->{$array_key}}
+    push @results, @{$ref->{query}->{$list}->{$array_key}}
 
-  } until ( ! $do_continue || ($count>$max && $max != 0) );
+  } until ( ! $do_continue || ($count>=$max && $max != 0) );
 
-  return @list_array;
+  return @results;
 }
 
 sub get_page {
@@ -82,8 +106,11 @@ sub get_page {
 sub _error {
   my ($mw, $code, $desc) = @_;
   $mw->{error} = $code;
-  $mw->{error_desc} = $desc;
-  return $code;
+  $mw->{error_details} = $desc;
+
+  $mw->{on_error}->() if ($mw->{on_error});
+
+  return undef;
 }
 
 1;
--- testmodule.pl
+++ testmodule.pl
@@ -6,17 +6,13 @@ use Data::Dumper;
 
 my $mw = MediaWiki::API->new();
 $mw->{config}->{api_url} = 'http://www.exotica.org.uk/mediawiki/api.php';
+$mw->{config}->{limits} = 'max';
 
-
-
-#api.php ? action=query & list=categorymembers & cmtitle=Category:Physics & cmsort=timestamp & cmdir=desc
-
-print Dumper $mw->list ( { action => 'query', list => 'allpages' }, 50 );
-print Dumper $mw->list ( { action => 'query', list => 'categorymembers', cmtitle => 'Category:Arcade Conversions' }, 50 );
-
-
-#$mw->api( { action => 'login', lgname => 'blah', lgpassword => 'blah' } );
-#$mw->api( { action => 'query', list => 'categorymembers', cmtitle => 'Category:Arcade_Conversions' } );
+if (!$mw->login( {lgname => 'blah', lgpassword => 'blah' } ) ) {
+  print $mw->{error_details}.'\n';
+}
+print Dumper $mw->list ( { action => 'query', list => 'allpages', aplimit=>5 }, 10 );
+print Dumper $mw->list ( { action => 'query', list => 'categorymembers', cmtitle => 'Category:Arcade Conversions' }, 20 );
 
 #my $page=$mw->get_page('Main Page','content');
 #print $page->{content};
