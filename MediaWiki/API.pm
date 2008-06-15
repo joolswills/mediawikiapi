6faa60f46e9c08e5ebd907c45629c22d101eee57
Jools Wills <buzz@exotica.org.uk>
2008-06-15 13:35:02 +0000

    added list function which returns an array of hashrefs. this allows all the mediawiki list functions to be used with ease

--- MediaWiki/API.pm
+++ MediaWiki/API.pm
@@ -44,9 +44,38 @@ sub api {
   return $ref;
 }
 
+sub list {
+  my ($self, $query, $max) = @_;
+  my ($results, @list_array);
+  my ($cont_key, $cont_value, $array_key, $count);
+
+  my $list = $query->{list};
+
+  my $do_continue = 0;
+  do {
+    $results = $self->api( $query );
+
+    if ( ! defined( $array_key ) ) { 
+      ($array_key,)=each( %{ $results->{query}->{$list} } );
+    }
+    $count +=  scalar @{$results->{query}->{$list}->{$array_key}};
+
+    if ( exists( $results->{'query-continue'} ) ) {
+      # get query-continue hashref and extract key and value (key will be used as from parameter)
+      ($cont_key, $cont_value) = each( %{ $results->{'query-continue'}->{$list} } );
+      $query->{$cont_key} = $cont_value;
+      $do_continue=1;
+    }
+    push @list_array, @{$results->{query}->{$list}->{$array_key}}
+
+  } until ( ! $do_continue || ($count>$max && $max != 0) );
+
+  return @list_array;
+}
+
 sub get_page {
   my ($self, $page, $rvprop) = @_;
-  my $query = ($self->api( { action => 'query', prop => 'revisions', titles => $page, rvprop => $rvprop } ));
+  my $query = $self->api( { action => 'query', prop => 'revisions', titles => $page, rvprop => $rvprop } );
   return $query->{query}->{pages}->{page}->{revisions}->{rev};
 }
 
--- testmodule.pl
+++ testmodule.pl
@@ -5,9 +5,18 @@ use MediaWiki::API;
 use Data::Dumper;
 
 my $mw = MediaWiki::API->new();
-$mw->{config}->{api_url} = 'http://testwiki.exotica.org.uk/mediawiki/api.php';
+$mw->{config}->{api_url} = 'http://www.exotica.org.uk/mediawiki/api.php';
+
+
+
+#api.php ? action=query & list=categorymembers & cmtitle=Category:Physics & cmsort=timestamp & cmdir=desc
+
+print Dumper $mw->list ( { action => 'query', list => 'allpages' }, 50 );
+print Dumper $mw->list ( { action => 'query', list => 'categorymembers', cmtitle => 'Category:Arcade Conversions' }, 50 );
+
+
 #$mw->api( { action => 'login', lgname => 'blah', lgpassword => 'blah' } );
 #$mw->api( { action => 'query', list => 'categorymembers', cmtitle => 'Category:Arcade_Conversions' } );
 
-my $page=$mw->get_page('Main Page','content');
-print $page->{content};
+#my $page=$mw->get_page('Main Page','content');
+#print $page->{content};
