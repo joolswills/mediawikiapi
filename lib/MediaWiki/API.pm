7aade365802fa5a11bdf6fef7359e7b472e7c477
Jools Wills <buzz@exotica.org.uk>
2008-06-17 14:05:17 +0000

    changed structure and added perl module templates to make it a real module

--- lib/MediaWiki/API.pm
+++ lib/MediaWiki/API.pm
@@ -3,14 +3,9 @@ package MediaWiki::API;
 use warnings;
 use strict;
 
-#use LWP::Debug qw(+);
 use LWP::UserAgent;
 use XML::Simple qw(:strict);
 use HTML::Entities;
-
-use Data::Dumper;
-
-our($VERSION) = "0.2";
 
 use constant {
   ERR_NO_ERROR => 0,
@@ -21,6 +16,45 @@ use constant {
   ERR_EDIT     => 5,
   ERR_UPLOAD   => 6,
 };
+
+=head1 NAME
+
+MediaWiki::API - The great new MediaWiki::API!
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION  = "0.2";
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use MediaWiki::API;
+
+    my $foo = MediaWiki::API->new();
+    ...
+
+=head1 EXPORT
+
+A list of functions that can be exported.  You can delete this section
+if you don't export anything, such as for a purely object-oriented module.
+
+=head1 FUNCTIONS
+
+=head2 function1
+
+=cut
+
+
+=head2 function2
+
+=cut
 
 sub new {
 
@@ -226,9 +260,58 @@ __END__
 
 =head1 AUTHOR
 
-Jools 'BuZz' Smyth <buzz@exotica.org.uk>
-http://www.exotica.org.uk
+Jools Smyth, C<< <buzz at exotica.org.uk> >>
 
-=head1 COPYRIGHT
+=head1 BUGS
 
-Copyright (C) 2008 Jools Smyth
+Please report any bugs or feature requests to C<bug-mediawiki-api at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=MediaWiki-API>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc MediaWiki::API
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=MediaWiki-API>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/MediaWiki-API>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/MediaWiki-API>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/MediaWiki-API>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2008 Jools Smyth, all rights reserved.
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+
+=cut
+
+1; # End of MediaWiki::API
