55c8a6975ce6621484dc57ade7ff78da96b40a9b
Jools Wills <buzz@exotica.org.uk>
2008-07-30 15:53:59 +0000

    bumped version to 0.12 so as to get a fixed version on cpan without the debug line

--- lib/MediaWiki/API.pm
+++ lib/MediaWiki/API.pm
@@ -27,11 +27,11 @@ MediaWiki::API - Provides a Perl interface to the MediaWiki API (http://www.medi
 
 =head1 VERSION
 
-Version 0.11
+Version 0.12
 
 =cut
 
-our $VERSION  = "0.11";
+our $VERSION  = "0.12";
 
 =head1 SYNOPSIS
 
